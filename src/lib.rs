b38dae20777a01f0915bbd6e8bc53e62e7ef3569
Anthony McAtear <mcatear.anthony@gmail.com>
2023-04-09 23:48:31 +0200

    updating allocator due to issues with `cargo test`

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,21 @@ use wasm_bindgen::prelude::*;
 
 extern crate alloc;
 
-use lol_alloc::{AssumeSingleThreaded, FreeListAllocator};
+#[cfg(target_arch = "wasm32")]
+use lol_alloc::{FreeListAllocator, LockedAllocator};
 
-// SAFETY: This application is single threaded, so using AssumeSingleThreaded is allowed.
+#[cfg(target_arch = "wasm32")]
 #[global_allocator]
-static ALLOCATOR: AssumeSingleThreaded<FreeListAllocator> =
-    unsafe { AssumeSingleThreaded::new(FreeListAllocator::new()) };
+static ALLOCATOR: LockedAllocator<FreeListAllocator> = LockedAllocator::new(FreeListAllocator::new());
+
+// NOTE: This produced issues with `cargo test`
+//
+// use lol_alloc::{AssumeSingleThreaded, FreeListAllocator};
+//
+// // SAFETY: This application is single threaded, so using AssumeSingleThreaded is allowed.
+// #[global_allocator]
+// static ALLOCATOR: AssumeSingleThreaded<FreeListAllocator> =
+//     unsafe { AssumeSingleThreaded::new(FreeListAllocator::new()) };
 
 pub type DrawResult<T> = Result<T, Box<dyn std::error::Error>>;
 
